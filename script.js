bed321e1a7ad3166ce00cc61d651e76ff921abcc
Ashish Mythreya <enagandhulaashishmythreya@gmail.com>
2025-03-19 01:14:09 +0530

    Initial commit of my portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+// Alert for contact section
+document.querySelector('#contact').addEventListener('click', () => {
+    alert('Thanks for checking out my portfolio!');
+});
